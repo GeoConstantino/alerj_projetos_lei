23c0343565bfbd7c42edb1efa8c45de25abb6f70
Geovani Constantino <constantino@geovani-pc>
2019-10-11 13:31:12 -0300

    Added function to join the Law Project

--- main.py
+++ main.py
@@ -19,17 +19,31 @@ def get_data(row):
     data = row.findAll('font')[2].contents[0]
     return {'data': data.strip()}
 
+
 def get_autor(row):
     autor = row.findAll('font')[3].contents[0].strip().split(',')
     return {'autor': autor}
 
+
+def get_lei_full(row):
+
+    lei = get_num(row)
+    lei.update(get_lei(row))
+    lei.update(get_data(row))
+    lei.update(get_autor(row))
+
+    return lei
+
+
 if __name__ == "__main__":
-    
+
     page = requests.get(LINK)
     soup = BeautifulSoup(page.text, 'html.parser')
+
+    leis = list()
+
     for row in soup.findAll('table')[0].findAll('tr')[3::]:
         
-        import ipdb; ipdb.set_trace()
-        #row.findAll()
-        #for row_i in row.findAll('td')
+        lei = get_lei_full(row)
 
+        leis.append(lei)
--- tests.py
+++ tests.py
@@ -3,7 +3,7 @@ from bs4 import BeautifulSoup
 
 import requests
 
-from main import get_lei, get_num, get_data, get_autor
+from main import get_lei, get_num, get_data, get_autor, get_lei_full
 
 
 class ProjetoLei(TestCase):
@@ -53,3 +53,18 @@ class ProjetoLei(TestCase):
         alerj = get_autor(row[17])
        
         self.assertEqual(alerj['autor'], ['FLÁVIO SERAFINI','WALDECK CARNEIRO'])
+
+    def test_row_lei_full(self):
+
+        link = 'pagina_test/lei.html'
+        soup = BeautifulSoup(open(link), 'html.parser')
+        row = soup.findAll('table')[0].findAll('tr')[3::]
+        lei_full = get_lei_full(row[0])
+
+        self.assertEqual(lei_full['num'], '20190301423')
+        
+        self.assertEqual(lei_full['lei'], 'ALTERA A EMENTA DA LEI N° 3722, DE 26 DE NOVEMBRO DE 2001, QUE DETERMINA A INSTALAÇÃO DE CRECHES E BERÇARIOS NOS BATALHÕES DA POLÍCIA MILITAR DO ESTADO DO RIO DE JANEIRO PARA ATENDIMENTO DOS FILHOS DOS POLICIAIS MILITARES, E ACRESCENTA PARÁGRAFO ÚNICO AO SEU ARTIGO 1º => 20190301423}') 
+
+        self.assertEqual(lei_full['data'], '09/10/2019' )
+
+        self.assertEqual(lei_full['autor'], ['DIONISIO LINS'])
